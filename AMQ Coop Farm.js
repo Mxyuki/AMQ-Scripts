790b4518be550f52d465fab9e2dd750e37b553db
Mxyuki <86327219+Mxyuki@users.noreply.github.com>
2021-10-24 20:22:39 +0200

    Sending Answer Update
    Now sending answer will be done automaticaly when pressing eneter while responding.

--- AMQ Coop Farm.js
+++ AMQ Coop Farm.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         AMQ Co-op Farm V2
+// @name         AMQ Co-op Farm
 // @namespace    https://github.com/Mxyuki/
-// @version      0.2
+// @version      0.3
 // @description  Auto Farm respond and then press ↑ so everyone get the answer !
 // @author       Mxyuki
 // @match        https://animemusicquiz.com/*
@@ -10,10 +10,9 @@
 // @require      https://raw.githubusercontent.com/TheJoseph98/AMQ-Scripts/master/common/amqWindows.js
 // ==/UserScript==
 
-// don't load on login page
+
 if (document.getElementById("startPage")) return;
 
-// Wait until the LOADING... screen is hidden and load script
 let loadInterval = setInterval(() => {
     if (document.getElementById("loadingScreen").classList.contains("hidden")) {
         setup();
@@ -21,14 +20,13 @@ let loadInterval = setInterval(() => {
     }
 }, 500);
 
-
-
 let farmWindow;
-let on = 2;
+let mode = 2;
+var lastAnswer= '';
 var x = '';
 var ar = '';
+var prefix = '/// ';
 
-//Disable in ranked
 if (quiz.gameMode == "Ranked") return;
 
 function sendChatMessage(message) {
@@ -38,33 +36,32 @@ function sendChatMessage(message) {
 
 window.addEventListener("keydown", checkKeyPress, false)
 function checkKeyPress(key) {
-  if (key.keyCode == '38' && on == 1 || key.keyCode == '38' && on == 3) {
+  if (key.keyCode == '13' && mode == 1|| key.keyCode == '13' && mode == 3) {
 
-//Send Answer
     x = document.getElementById("qpAnswerInput").value;
-    sendChatMessage('/// ' + x);
+
+      if(x != '' && x != lastAnswer){
+    sendChatMessage(prefix + x);
+          lastAnswer = x;
+      }
   }
 }
 
-new Listener("Game Chat Message", (payload) => {
-  processChatCommand(payload);
-}).bindListener();
 new Listener("game chat update", (payload) => {
   payload.messages.forEach(message => {
     processChatCommand(message);
   });
 }).bindListener();
 
-
-//Auto Answer
 function processChatCommand(payload) {
-  if (payload.message.startsWith("///") && on == 1 || payload.message.startsWith("///") && on == 4 ) {
+  if (payload.message.startsWith(prefix) && mode == 1 || payload.message.startsWith(prefix) && mode == 4 ) {
     var index = payload.message.indexOf(' ');
     if (index > 0) ar = payload.message.substr(index + 1);
     else ar = '';
     if (quiz.isSpectator) return;
     if (ar.length > 0) {
       quiz.skipClicked();
+        //lastAnswer = ar;
       $("#qpAnswerInput").val(ar);
       quiz.answerInput.submitAnswer(true);
     }
@@ -74,25 +71,29 @@ function processChatCommand(payload) {
     }
 }
 
-//Change Status
+	new Listener("answer results", (data) => {
+		lastAnswer = "";
+        sendChatMessage(lastAnswer);
+	}).bindListener();
+
 function Enable(){
 $("#farmStatus").text('Enabled');
-    on = 1;
+    mode = 1;
 }
 
 function Disable(){
 $("#farmStatus").text('Disabled');
-    on = 2;
+    mode = 2;
 }
 
 function Send(){
 $("#farmStatus").text('Send Only');
-    on = 3;
+    mode = 3;
 }
 
 function Receive(){
 $("#farmStatus").text('Receive Only');
-    on = 4;
+    mode = 4;
 }
 
 function setup() {
@@ -181,7 +182,7 @@ function setup() {
         description: `
             <p>This script is made to make your game in Farm co-op easier.</p>
             <p>--- Buttons ---</p>
-            <p>↑ : Send your answer in chat, if someone as the script and activated auto answer it will answer for him.</p>
+            <p>Enter : If you have an answer writen pressing enter will permite to send it one time.
             <p>Top right "Coop Farm" button : Change Co-op Mode.</p>
             <p>--- Mods ---</p>
             <p>Co-op Farm enabled - Receive answer and Send answer Enabled.</p>
