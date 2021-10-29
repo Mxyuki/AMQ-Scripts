2a2910b75626ef7d7f9e3e68f42bf76ea6dc71de
Mxyuki <86327219+Mxyuki@users.noreply.github.com>
2021-10-29 11:15:03 +0200

    Added a margin of error
    Now in song name or artist name if you miss a letter it will be count as good

--- AMQ ArtistSongTypeGuess.js
+++ AMQ ArtistSongTypeGuess.js
@@ -48,6 +48,57 @@ function sendChatMessage(message) {
     gameChat.sendMessage();
 }
 
+function similarity(s1, s2) {
+  var longer = s1;
+  var shorter = s2;
+  if (s1.length < s2.length) {
+    longer = s2;
+    shorter = s1;
+  }
+  var longerLength = longer.length;
+  if (longerLength == 0) {
+    return 1.0;
+  }
+  return (longerLength - editDistance(longer, shorter)) / parseFloat(longerLength);
+}
+
+function editDistance(s1, s2) {
+  s1 = s1.toLowerCase();
+  s2 = s2.toLowerCase();
+
+  var costs = new Array();
+  for (var i = 0; i <= s1.length; i++) {
+    var lastValue = i;
+    for (var j = 0; j <= s2.length; j++) {
+      if (i == 0)
+        costs[j] = j;
+      else {
+        if (j > 0) {
+          var newValue = costs[j - 1];
+          if (s1.charAt(i - 1) != s2.charAt(j - 1))
+            newValue = Math.min(Math.min(newValue, lastValue),
+              costs[j]) + 1;
+          costs[j - 1] = lastValue;
+          lastValue = newValue;
+        }
+      }
+    }
+    if (i > 0)
+      costs[s2.length] = lastValue;
+  }
+  return costs[s2.length];
+}
+
+
+
+
+
+
+
+
+
+
+
 function setup() {
     new Listener("Game Starting", (payload) =>{
         songPoint = 0;
@@ -96,14 +147,17 @@ function setup() {
             artist = (artist.toLowerCase());
             opedins = (opedins.toLowerCase());
 
-                if(songAnswer == song && toggle == 1){
+            var songSim = similarity(song,songAnswer)
+            var artistSim = similarity(artist,artistAnswer)
+
+                if(songAnswer == song && toggle == 1 || songSim >= 0.75 && toggle == 1){
                     songPoint++;
                     sendChatMessage("Song Point +1, Total :" + songPoint + "/" + countSong );
                 }
 
-                if(artistAnswer == artist && toggle == 1){
+                if(artistAnswer == artist && toggle == 1 || artistSim >= 0.75 && toggle == 1 ){
                     artistPoint++;
-                    sendChatMessage("Artist Point +1, Total :" + artistPoint + "/" + countSong );
+                    sendChatMessage("Artist Point +1, Total :" + artistPoint + "/" + countSong);
                 }
 
                 if(opedinsAnswer == opedins && toggle == 1){
