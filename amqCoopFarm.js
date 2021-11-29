f0b75c8eda8f21b9c9d5499ef09891edca8e0b45
Mxyuki <86327219+Mxyuki@users.noreply.github.com>
2021-11-29 20:14:09 +0100

    Update amqCoopFarm.js

--- amqCoopFarm.js
+++ amqCoopFarm.js
@@ -5,7 +5,7 @@
 // @description  Auto Farm for AMQ. Answer and it will send it in the chatm everyone with the script will have it writen automaticaly.
 // @author       Mxyuki
 // @match        https://animemusicquiz.com/*
-// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @updateUrl    https://github.com/Mxyuki/AMQ-Scripts/raw/master/AMQArtistSongTypeGuess.js
 // @require      https://raw.githubusercontent.com/TheJoseph98/AMQ-Scripts/master/common/amqScriptInfo.js
 // @require      https://raw.githubusercontent.com/TheJoseph98/AMQ-Scripts/master/common/amqWindows.js
 // ==/UserScript==
@@ -31,65 +31,66 @@ var chat;
 if (quiz.gameMode == "Ranked") return;
 
 function sendChatMessage(message) {
-  gameChat.$chatInputField.val(message);
-  gameChat.sendMessage();
+    gameChat.$chatInputField.val(message);
+    gameChat.sendMessage();
 }
 
 window.addEventListener("keydown", checkKeyPress, false)
 function checkKeyPress(key) {
-  if (key.keyCode == '13' && mode == 1|| key.keyCode == '13' && mode == 3) {
+    if (key.keyCode == '13' && mode == 1|| key.keyCode == '13' && mode == 3) {
 
-    x = document.getElementById("qpAnswerInput").value;
-    chat = document.getElementById("gcInput").value;
+        x = document.getElementById("qpAnswerInput").value;
+        chat = document.getElementById("gcInput").value;
 
-      if(x != '' && x != lastAnswer && chat == ''){
-    sendChatMessage(prefix + x);
-          lastAnswer = x;
-      }
-  }
+        if(x != '' && x != lastAnswer && chat == ''){
+            sendChatMessage(prefix + x);
+            lastAnswer = x;
+        }
+    }
 }
 
 new Listener("game chat update", (payload) => {
-  payload.messages.forEach(message => {
-    processChatCommand(message);
-  });
+    payload.messages.forEach(message => {
+        processChatCommand(message);
+    });
 }).bindListener();
 
 function processChatCommand(payload) {
-  if (payload.message.startsWith(prefix) && mode == 1 || payload.message.startsWith(prefix) && mode == 4 ) {
-    var index = payload.message.indexOf(' ');
-    if (index > 0) ar = payload.message.substr(index + 1);
-    else ar = '';
-    if (quiz.isSpectator) return;
-    if (ar.length > 0) {
-      quiz.skipClicked();
-        lastAnswer = ar;
-      $("#qpAnswerInput").val(ar);
-      quiz.answerInput.submitAnswer(true);
+    if (payload.message.startsWith(prefix) && mode == 1 || payload.message.startsWith(prefix) && mode == 4 ) {
+        var index = payload.message.indexOf(' ');
+        if (index > 0) ar = payload.message.substr(index + 1);
+        else ar = '';
+        if (quiz.isSpectator) return;
+        if (ar.length > 0) {
+            quiz.skipClicked();
+            lastAnswer = ar;
+            $("#qpAnswerInput").val(ar);
+            quiz.answerInput.submitAnswer(true);
+        }
     }
-  }
 
-	new Listener("answer results", (data) => {
-		lastAnswer = "";
-	}).bindListener();
+    new Listener("answer results", (data) => {
+        lastAnswer = "";
+
+    }).bindListener();
 }
 function Enable(){
-$("#farmStatus").text('Enabled');
+    $("#farmStatus").text('Enabled');
     mode = 1;
 }
 
 function Disabled(){
-$("#farmStatus").text('Disabled');
+    $("#farmStatus").text('Disabled');
     mode = 2;
 }
 
 function Send(){
-$("#farmStatus").text('Send Only');
+    $("#farmStatus").text('Send Only');
     mode = 3;
 }
 
 function Receive(){
-$("#farmStatus").text('Receive Only');
+    $("#farmStatus").text('Receive Only');
     mode = 4;
 }
 
@@ -118,7 +119,7 @@ function setup() {
         id: "farmWindowResults"
     });
 
-   farmWindow.panels[0].panel.append(
+    farmWindow.panels[0].panel.append(
         $(`<div id="farmWindowButton"></div>`)
         .append(
             $(`<button id="farmWindowEnable" class="btn btn-primary">Enable</button>`).click(function () {
@@ -142,7 +143,7 @@ function setup() {
         )
     );
 
-   farmWindow.panels[1].panel.append(
+    farmWindow.panels[1].panel.append(
         $(`<div id="farmerWindowStatus"></div>`)
         .append(
             $(
@@ -156,27 +157,27 @@ function setup() {
     let oldWidth = $("#qpOptionContainer").width();
     $("#qpOptionContainer").width(oldWidth + 35);
     $("#qpOptionContainer > div").append($(`<div id="qpFarmCoop" class="clickAble qpOption"><i aria-hidden="true" class="fa fa-bars qpMenuItem"></i></div>`)
-        .click(() => {
-            if (farmWindow.isVisible()) {
-                farmWindow.close();
-            }
-            else {
-                farmWindow.open();
-            }
-        })
-        .popover({
-            content: "Coop Farm",
-            trigger: "hover",
-            placement: "bottom"
-        })
-    );
+                                         .click(() => {
+        if (farmWindow.isVisible()) {
+            farmWindow.close();
+        }
+        else {
+            farmWindow.open();
+        }
+    })
+                                         .popover({
+        content: "Coop Farm",
+        trigger: "hover",
+        placement: "bottom"
+    })
+                                        );
 
 }
 
-    AMQ_addScriptData({
-        name: "Co-op Farm",
-        author: "Mxyuki",
-        description: `
+AMQ_addScriptData({
+    name: "Co-op Farm",
+    author: "Mxyuki",
+    description: `
             <p>This script is made to make your game in Farm co-op easier.</p>
             <p>--- Buttons ---</p>
             <p>Enter : If you have an answer writen pressing enter will permite to send it one time.
@@ -188,9 +189,9 @@ function setup() {
             <p>Co-op Farm Disabled - Receive answer and Send answer are Disabled.</p>
         `
 
-    });
+});
 
-    AMQ_addStyle(`
+AMQ_addStyle(`
         #qpFarmCoop {
             width: 27px;
             margin-right: 5px;
