91c0367430317954fb978952618fcef3ca6ff48b
Mxyuki <86327219+Mxyuki@users.noreply.github.com>
2022-08-04 18:30:00 +0200

    Update amqCoopFarm.user.js

--- amqCoopFarm.user.js
+++ amqCoopFarm.user.js
@@ -8,6 +8,7 @@
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @require      https://raw.githubusercontent.com/TheJoseph98/AMQ-Scripts/master/common/amqScriptInfo.js
 // @require      https://raw.githubusercontent.com/TheJoseph98/AMQ-Scripts/master/common/amqWindows.js
+// @updateURL	 https://github.com/Mxyuki/AMQ-Scripts/raw/master/amqCoopFarm.user.js
 // ==/UserScript==
 
 
