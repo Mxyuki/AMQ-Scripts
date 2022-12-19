c1f5f91f09d1dcf80316cdc79d2491f5aac9f447
Mxyuki <86327219+Mxyuki@users.noreply.github.com>
2022-12-19 20:53:47 +0100

    Very early stage juste trying different things

---
+++ eloBot.user.js
@@ -0,0 +1,48 @@
+// ==UserScript==
+// @name         EloBot
+// @namespace    https://github.com/Mxyuki/AMQ-Scripts
+// @version      0.1
+// @description  Bored so i try to make a Bot that make an elo system room
+// @author       Mxyuki
+// @match        https://animemusicquiz.com/
+// @grant        none
+// ==/UserScript==
+
+let eloList =[];
+let playerList =[];
+
+if (document.getElementById("startPage")) return;
+
+new Listener("New Player", (payload) => {
+    let name = payload.name;
+    let id = payload.gamePlayerId;
+    playerList.push({name: name, id: id});
+    if(name != "Miyuki_Damon"){
+        let player = eloList.find(p => p.name === name);
+        if(player){
+            console.log("name: " + name);
+            console.log("id:   " + id);
+            console.log("elo:  " + player.elo);
+        }
+        else{
+            eloList.push({name: name, elo: 0});
+        }
+    }
+}).bindListener();
+
+new Listener("Spectator Change To Player", (payload) => {
+    let name = payload.name;
+    let id = payload.gamePlayerId;
+    playerList.push({name: name, id: id});
+    if(name != "Miyuki_Damon"){
+        let player = eloList.find(p => p.name === name);
+        if(player){
+            console.log("name: " + name);
+            console.log("id:   " + id);
+            console.log("elo:  " + player.elo);
+        }
+        else{
+            eloList.push({name: name, elo: 0});
+        }
+    }
+}).bindListener();
