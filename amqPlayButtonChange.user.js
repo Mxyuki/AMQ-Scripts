e7bd580da7020437d06775d40aa25575a286fdac
Mxyuki <86327219+Mxyuki@users.noreply.github.com>
2022-12-24 06:02:30 +0100

    0.2

--- amqPlayButtonChange.user.js
+++ amqPlayButtonChange.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AMQ PlayButton Change
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Change how the Solo, Multi and Nexus button are diplayed
 // @author       Mxyuki
 // @match        https://animemusicquiz.com/
@@ -63,10 +63,51 @@ styleSheet.insertRule('.miyuButton { width: 33.33%; background-color: transparen
 
 styleSheet.insertRule('#mainMenu.button { display: flex; }', styleSheet.cssRules.length);
 
+mpPlayButton.style.backgroundSize = 'cover';
+
 const buttons = document.querySelectorAll('#mainMenu > .button');
 
 // Iterate over the elements and change their CSS
 buttons.forEach(button => {
   button.style.display = 'flex';
   button.style.marginLeft = '15%';
+});
+
+// Change the background for hovering Solo button
+soloButton.addEventListener('mouseover', () => {
+
+  // Set the background of the element to the image URL
+  mpPlayButton.style.background = `url("https://i.imgur.com/7F2yuIk.png")`;
+});
+
+soloButton.addEventListener('mouseout', () => {
+
+  // Remove the background image from the element
+  mpPlayButton.style.background = '';
+});
+
+// Change the background for hovering Multi button
+multiButton.addEventListener('mouseover', () => {
+
+  // Set the background of the element to the image URL
+  mpPlayButton.style.background = `url("https://i.imgur.com/qtN8pG5.png")`;
+});
+
+multiButton.addEventListener('mouseout', () => {
+
+  // Remove the background image from the element
+  mpPlayButton.style.background = '';
+});
+
+// Change the background for hovering Nexus button
+nexusButton.addEventListener('mouseover', () => {
+
+  // Set the background of the element to the image URL
+  mpPlayButton.style.background = `url("https://i.imgur.com/hLQvYb3.png")`;
+});
+
+nexusButton.addEventListener('mouseout', () => {
+
+  // Remove the background image from the element
+  mpPlayButton.style.background = '';
 });
