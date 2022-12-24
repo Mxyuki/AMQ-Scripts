bb080b7ae3b5655dfcc594b89ab5ece3f1487b22
Mxyuki <86327219+Mxyuki@users.noreply.github.com>
2022-12-24 07:37:20 +0100

    0.3

--- amqPlayButtonChange.user.js
+++ amqPlayButtonChange.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AMQ PlayButton Change
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  Change how the Solo, Multi and Nexus button are diplayed
 // @author       Mxyuki
 // @match        https://animemusicquiz.com/
@@ -9,105 +9,55 @@
 
 if (document.getElementById("startPage")) return;
 
-// Select the mpPlayButton element
 const mpPlayButton = document.getElementById('mpPlayButton');
 
-// Remove the data-toggle and data-target attributes
 mpPlayButton.removeAttribute('data-toggle');
 mpPlayButton.removeAttribute('data-target');
 
-// Select the h1 element and remove it from the mpPlayButton element
 const h1Element = mpPlayButton.querySelector('h1');
 mpPlayButton.removeChild(h1Element);
 
-// Create the Solo button
-const soloButton = document.createElement('button');
-soloButton.textContent = 'Solo';
-soloButton.onclick = () => { hostModal.displayHostSolo(); };
-soloButton.classList.add('miyuButton');
-soloButton.id = 'miyuSolo';
+const soloButton = createButton('Solo', () => hostModal.displayHostSolo());
+const multiButton = createButton('Multi', () => viewChanger.changeView('roomBrowser'));
+const nexusButton = createButton('Nexus', () => viewChanger.changeView('nexus'));
 
-// Create the Multi button
-const multiButton = document.createElement('button');
-multiButton.textContent = 'Multi';
-multiButton.onclick = () => { viewChanger.changeView('roomBrowser'); };
-multiButton.classList.add('miyuButton');
-multiButton.id = 'miyuMulti';
-
-// Create the Nexus button
-const nexusButton = document.createElement('button');
-nexusButton.textContent = 'Nexus';
-nexusButton.onclick = () => { viewChanger.changeView('nexus'); };
-nexusButton.classList.add('miyuButton');
-nexusButton.id = 'miyuNexus';
-
-// Append the buttons to the mpPlayButton element
-mpPlayButton.appendChild(soloButton);
-mpPlayButton.appendChild(multiButton);
-mpPlayButton.appendChild(nexusButton);
-
-// Add some styles to the buttons to make them take up equal space and align horizontally
+mpPlayButton.append(soloButton, multiButton, nexusButton);
 mpPlayButton.style.display = 'flex';
 mpPlayButton.style.justifyContent = 'space-between';
 
-mpPlayButton.style.removeProperty('display');
+addButtonHoverStyles(soloButton, 'https://i.imgur.com/ycmLeXE.png');
+addButtonHoverStyles(multiButton, 'https://i.imgur.com/D5PGM29.png');
+addButtonHoverStyles(nexusButton, 'https://i.imgur.com/idgLUtS.png');
 
-// Select the style element that you want to add the rule to
 const styleElement = document.querySelector('style');
-
-// Get the CSSStyleSheet object for the style element
 const styleSheet = styleElement.sheet;
 
-// Add the rule to the style sheet
 styleSheet.insertRule('.miyuButton { width: 33.33%; background-color: transparent; border: none; }', styleSheet.cssRules.length);
-
 styleSheet.insertRule('#mainMenu.button { display: flex; }', styleSheet.cssRules.length);
 
 mpPlayButton.style.backgroundSize = 'cover';
 
 const buttons = document.querySelectorAll('#mainMenu > .button');
 
-// Iterate over the elements and change their CSS
 buttons.forEach(button => {
   button.style.display = 'flex';
   button.style.marginLeft = '15%';
 });
 
-// Change the background for hovering Solo button
-soloButton.addEventListener('mouseover', () => {
+function createButton(text, onclick) {
+  const button = document.createElement('button');
+  button.textContent = text;
+  button.onclick = onclick;
+  button.classList.add('miyuButton');
+  button.id = `miyu${text}`;
+  return button;
+}
 
-  // Set the background of the element to the image URL
-  mpPlayButton.style.background = `url("https://i.imgur.com/7F2yuIk.png")`;
-});
-
-soloButton.addEventListener('mouseout', () => {
-
-  // Remove the background image from the element
-  mpPlayButton.style.background = '';
-});
-
-// Change the background for hovering Multi button
-multiButton.addEventListener('mouseover', () => {
-
-  // Set the background of the element to the image URL
-  mpPlayButton.style.background = `url("https://i.imgur.com/qtN8pG5.png")`;
-});
-
-multiButton.addEventListener('mouseout', () => {
-
-  // Remove the background image from the element
-  mpPlayButton.style.background = '';
-});
-
-// Change the background for hovering Nexus button
-nexusButton.addEventListener('mouseover', () => {
-
-  // Set the background of the element to the image URL
-  mpPlayButton.style.background = `url("https://i.imgur.com/hLQvYb3.png")`;
-});
-
-nexusButton.addEventListener('mouseout', () => {
-
-  // Remove the background image from the element
-  mpPlayButton.style.background = '';
-});
+function addButtonHoverStyles(button, hoverUrl) {
+  button.addEventListener('mouseover', () => {
+    mpPlayButton.style.background = `url("${hoverUrl}")`;
+  });
+  button.addEventListener('mouseout', () => {
+    mpPlayButton.style.background = '';
+  });
+}
