605be7614800413ec74f9f68211592f0c0cd77ae
Micookie <86327219+Mxyuki@users.noreply.github.com>
2022-12-30 21:49:19 +0100

    0.2
    Fixed Major Bug

--- amqDMSave.user.js
+++ amqDMSave.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AMQ DM Save
 // @namespace    https://github.com/Mxyuki/AMQ-Scripts
-// @version      0.1
+// @version      0.2
 // @description  Save the last 5 messages you sent in dm
 // @author       Mxyuki
 // @match        https://animemusicquiz.com/*
@@ -16,75 +16,110 @@ let messages = savedMessages ? JSON.parse(savedMessages) : {};
 
 let firstMessage = [];
 
+let message1;
+let message2;
+let message3;
+let message4;
+let message5;
+
 new Listener("chat message", (receive) => {
 
     if (!firstMessage.includes(receive.sender)) {
-        console.log(`${receive.sender} added`);
-        loadMessages(receive.sender);
+
         firstMessage.push(receive.sender);
+
+        if(messages[receive.sender]){
+
+            console.log(`${receive.sender} added`);
+            loadMessages(receive.sender);
+        }
     }
 
-    if (!messages[receive.sender]) {
-        messages[receive.sender] = [];
-    }
+    if (!messages[receive.sender]) messages[receive.sender] = [];
 
     messages[receive.sender].push({
         sender: receive.sender,
         message: receive.message
     });
 
-    if (messages[receive.sender].length > 5) {
-        messages[receive.sender].shift();
-    }
+    if (messages[receive.sender].length > 5) messages[receive.sender].shift();
 
     localStorage.setItem("messages", JSON.stringify(messages));
 
-    console.log(messages[receive.sender]);
 }).bindListener();
 
 new Listener("chat message response", (send) => {
 
     if (!firstMessage.includes(send.target)) {
-        console.log(`${send.target} added`);
-        loadMessages(send.target);
+
         firstMessage.push(send.target);
+
+        if(messages[send.target]){
+
+            console.log(`${send.target} added`);
+            loadMessages(send.target);
+        }
     }
 
-    if (!messages[send.target]) {
-        messages[send.target] = [];
-    }
+    if (!messages[send.target]) messages[send.target] = [];
 
     messages[send.target].push({
         sender: selfName,
         message: send.msg
     });
 
-    if (messages[send.target].length > 5) {
-        messages[send.target].shift();
-    }
+    if (messages[send.target].length > 5) messages[send.target].shift();
 
     localStorage.setItem("messages", JSON.stringify(messages));
 
-    console.log(messages[send.target]);
 }).bindListener();
 
 function loadMessages(name){
 
-    console.log(name);
+    if(messages[name].length >= 1) message1 = messages[name][0].sender + ": " + messages[name][0].message;
+    if(messages[name].length >= 2) message2 = messages[name][1].sender + ": " + messages[name][1].message;
+    if(messages[name].length >= 3) message3 = messages[name][2].sender + ": " + messages[name][2].message;
+    if(messages[name].length >= 4) message4 = messages[name][3].sender + ": " + messages[name][3].message;
+    if(messages[name].length >= 5) message5 = messages[name][4].sender + ": " + messages[name][4].message;
 
-    let message1 = messages[name][0].sender + ": " + messages[name][0].message;
-    let message2 = messages[name][1].sender + ": " + messages[name][1].message;
-    let message3 = messages[name][2].sender + ": " + messages[name][2].message;
-    let message4 = messages[name][3].sender + ": " + messages[name][3].message;
-    let message5 = messages[name][4].sender + ": " + messages[name][4].message;
 
-    setTimeout(function() {
-        $(`#chatBox-${name} > .chatBoxContainer > .chatContent > .ps__scrollbar-y-rail`).after(
-            `<li>${message1}</li>
-             <li>${message2}</li>
-             <li>${message3}</li>
-             <li>${message4}</li>
-             <li>${message5}</li>`
-        );
-    }, 100);
+    if(messages[name].length >= 5){
+        setTimeout(function() {
+            $(`#chatBox-${name} > .chatBoxContainer > .chatContent > .ps__scrollbar-y-rail`).after(
+                `<li>${message5}</li>`
+            );
+        }, 20);
+    }
+
+    if(messages[name].length >= 4){
+        setTimeout(function() {
+            $(`#chatBox-${name} > .chatBoxContainer > .chatContent > .ps__scrollbar-y-rail`).after(
+                `<li>${message4}</li>`
+            );
+        }, 20);
+    }
+
+    if(messages[name].length >= 3){
+        setTimeout(function() {
+            $(`#chatBox-${name} > .chatBoxContainer > .chatContent > .ps__scrollbar-y-rail`).after(
+                `<li>${message3}</li>`
+            );
+        }, 20);
+    }
+
+    if(messages[name].length >= 2){
+        setTimeout(function() {
+            $(`#chatBox-${name} > .chatBoxContainer > .chatContent > .ps__scrollbar-y-rail`).after(
+                `<li>${message2}</li>`
+            );
+        }, 20);
+    }
+
+    if(messages[name].length >= 1){
+        setTimeout(function() {
+            $(`#chatBox-${name} > .chatBoxContainer > .chatContent > .ps__scrollbar-y-rail`).after(
+                `<li>${message1}</li>`
+            );
+        }, 20);
+    }
 }
