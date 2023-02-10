ba80ace8f9394eac8b83783c4eb786666137b85f
Micookie <86327219+Mxyuki@users.noreply.github.com>
2023-02-10 02:37:09 +0100

    0.5

--- amqAutoSendAnswer.user.js
+++ amqAutoSendAnswer.user.js
@@ -6,40 +6,28 @@
 // @author       Mxyuki
 // @match        https://animemusicquiz.com/
 // ==/UserScript==
-
-const startPageElement = document.getElementById('startPage');
-if (startPageElement) {
-  return;
-}
+if (document.getElementById("startPage")) return
 
 let isAutoSend = false;
-let canWrite = true;
 
 document.addEventListener('keyup', (event) => {
-  if (event.altKey && event.keyCode === 84) {
-    isAutoSend = !isAutoSend;
-    gameChat.systemMessage(isAutoSend ? "Auto Send is Enabled. Press [ALT+T] to disable." : "Auto Send is Disabled. Press [ALT+T] to enable.");
-  } else if (event.keyCode >= 0 && isAutoSend && canWrite) {
-    quiz.answerInput.submitAnswer(true);
-    const answerInput = document.getElementById("qpAnswerInput");
-    const answer = answerInput.value;
-    if (answer === "" && event.keyCode === 8) {
-      answerInput.value = " ";
-      quiz.answerInput.submitAnswer(true);
-      answerInput.value = "";
+    if (event.altKey && event.keyCode === 84) {
+        isAutoSend = !isAutoSend;
+        if (!gameChat.isShown()) return;
+        gameChat.systemMessage(isAutoSend ? "Auto Send is Enabled. Press [ALT+T] to disable." : "Auto Send is Disabled. Press [ALT+T] to enable.");
     }
-  }
+    else if(event.keyCode === 13 && isAutoSend && document.getElementById("qpAnswerInput").value != ""){
+        quiz.answerInput.submitAnswer(true);
+    }
 });
 
-new Listener("play next song", (payload) => {
-  canWrite = true;
-}).bindListener();
-
-new Listener("guess phase over", (payload) => {
-  canWrite = false;
-}).bindListener();
-
-function chatSystemMessage(msg) {
-  if (!gameChat.isShown()) return;
-  gameChat.systemMessage(msg);
-}
+document.getElementById("qpAnswerInput").oninput = function() {
+    if(isAutoSend){
+        quiz.answerInput.submitAnswer(true);
+        if(document.getElementById("qpAnswerInput").value == ""){
+            document.getElementById("qpAnswerInput").value = " ";
+            quiz.answerInput.submitAnswer(true);
+            document.getElementById("qpAnswerInput").value = ""
+        }
+    }
+};
