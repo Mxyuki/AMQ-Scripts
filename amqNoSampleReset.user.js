841d9b64d3d76b748d7eefbfcc0d46bd8e8d407a
Micookie <86327219+Mxyuki@users.noreply.github.com>
2023-07-20 10:42:09 +0200

    Add that the song loop.

--- amqNoSampleReset.user.js
+++ amqNoSampleReset.user.js
@@ -1,10 +1,19 @@
 // ==UserScript==
 // @name         AMQ No Sample Reset
 // @namespace    https://github.com/Mxyuki/AMQ-Scripts
-// @version      0.1
-// @description  When guessing phase end doesn't restart from the start sample but continue the music
+// @version      0.2
+// @description  When guessing phase end doesn't restart from the start sample but continue the music.
 // @author       Mxyuki
 // @match        https://animemusicquiz.com/*
 // @icon         https://i.imgur.com/syptORo.png
 // ==/UserScript==
-if(document.getElementById("startPage"))return;new Listener("answer results",(payload)=>{let endTime=parseFloat(quizVideoController.moePlayers[quizVideoController.currentMoePlayerId].$player[0].currentTime.toFixed(2));setTimeout(function(){quizVideoController.moePlayers[quizVideoController.currentMoePlayerId].$player[0].currentTime=endTime;},0.0001);}).bindListener();
+
+if(document.getElementById("startPage"))return;
+
+new Listener("answer results",(payload)=>{
+    let endTime= parseFloat(quizVideoController.moePlayers[quizVideoController.currentMoePlayerId].$player[0].currentTime.toFixed(2));
+    setTimeout(function(){
+        quizVideoController.moePlayers[quizVideoController.currentMoePlayerId].$player[0].currentTime=endTime;
+        quizVideoController.moePlayers[quizVideoController.currentMoePlayerId].$player[0].loop = true;
+    },0.0001);
+}).bindListener();
