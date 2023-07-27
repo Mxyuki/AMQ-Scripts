7d19eea7713760181bce8a592e601c67cbbc93ba
Micookie <86327219+Mxyuki@users.noreply.github.com>
2023-07-27 21:42:17 +0200

    Update amqHiddenFriends.user.js

--- amqHiddenFriends.user.js
+++ amqHiddenFriends.user.js
@@ -1,67 +1,88 @@
 // ==UserScript==
 // @name         AMQ Hidden Friends
 // @namespace    https://github.com/Mxyuki/AMQ-Scripts
-// @version      0.1
-// @description  By doing /HF in game chat, it will inform you which of your friends are in invisible mode.
+// @version      1.0
+// @description  By doing /HF in game chat, it will inform you which of your friends are invisible to you, and when opening the friend list hiding friend while have a Blue statue with written hidden next to them.
 // @author       Mxyuki
 // @match        https://animemusicquiz.com/*
 // @icon         https://i.imgur.com/syptORo.png
+// @require      https://raw.githubusercontent.com/TheJoseph98/AMQ-Scripts/master/common/amqScriptInfo.js
 // ==/UserScript==
 
-let friendList = [];
 let onlinePlayer = [];
+let isCommand = false;
 
-// Wait for the game to load before setting up the script
+let command = "/HF";
+
 if (document.querySelector("#startPage")) return;
 
-// Listen for chat input and trigger hidingFriends when /HF is entered
 let chatBox = document.getElementById("gcInput");
 chatBox.addEventListener("keydown", function (event) {
     if (event.keyCode === 13) {
         let text = chatBox.value.trim();
-        if (text.startsWith("/HF")) {
+        if (text.startsWith(command)) {
             event.preventDefault();
-            hidingFriends();
+            isCommand = true;
+            socket.sendCommand({ type: "social", command: "get online users" });
             chatBox.value = "";
         }
     }
 });
 
-// Function to check for hidden friends and change their statue in the friend list
-function hidingFriends() {
-    document.getElementById("mainMenuSocailButton").click();
-    document.getElementById("mainMenuSocailButton").click();
-    setTimeout(function () {
-        onlinePlayer.forEach((name) => {
-            const friend = friendList.find((friend) => friend.name === name);
-            if (friend && !friend.online) {
-                gameChat.systemMessage(`${name} is hiding.`);
-                const playerNameContainers = document.querySelectorAll(".stPlayerNameContainer");
-                playerNameContainers.forEach((container) => {
-                    const playerNameElement = container.querySelector("h4");
-                    if (playerNameElement && playerNameElement.innerText === name) {
-                        const socialStatusCircle = container.closest("li").querySelector(".socialTabPlayerSocialStatusInnerCircle");
-                        if (socialStatusCircle) {
-                            socialStatusCircle.classList.remove("socialTabPlayerSocialStatusInnerCircleColorOffline");
-                            socialStatusCircle.style.backgroundColor = "lightblue";
-                            const statusElement = socialStatusCircle.closest("li").querySelector("h3");
-                            if (statusElement) {
-                                statusElement.textContent = "Hiding";
-                            }
-                        }
-                    }
-                });
-            }
-        });
-    }, 100);
-}
-
-// Listen for login completion to get the friendList
-new Listener("login complete", (payload) => {
-    friendList = payload.friends;
-}).bindListener();
-
-// Listen for online user updates to maintain the onlinePlayer array
 new Listener("all online users", (payload) => {
     onlinePlayer = payload;
+    let list = Object.keys(socialTab.offlineFriends).filter((name) => onlinePlayer.includes(name));
+    if(isCommand){
+        console.log(list);
+        if(list.length > 0){
+            list.forEach((hiddenPlayer) => {
+                gameChat.systemMessage(`${hiddenPlayer} is hiding.`);
+            });
+        }
+        else {
+            gameChat.systemMessage(`No one is hiding.`);
+        }
+        isCommand = false;
+    }
+    let friendElements = document.querySelectorAll(".socialTabFriendPlayerEntry");
+    friendElements.forEach((friendElement) => {
+        let nameElement = friendElement.querySelector(".stPlayerName h4");
+        let statusElement = friendElement.querySelector(".stPlayerName h3");
+        let statusCircleElement = friendElement.querySelector(".socialTabPlayerSocialStatusInnerCircle");
+        let friendName = nameElement.textContent.trim();
+        if (list.includes(friendName)) {
+            statusElement.textContent = "Hidden";
+            statusCircleElement.classList.remove("socialTabPlayerSocialStatusInnerCircleColorOffline");
+            statusCircleElement.classList.add("hiddenFriend");
+        } else {
+            if (statusCircleElement.classList.contains("hiddenFriend")) {
+                statusCircleElement.classList.remove("hiddenFriend");
+                statusCircleElement.classList.add("socialTabPlayerSocialStatusInnerCircleColorOffline");
+                statusElement.textContent = "Offline";
+            }
+        }
+    });
 }).bindListener();
+
+new Listener("friend state change", (payload) => {
+    socket.sendCommand({ type: "social", command: "get online users" });
+}).bindListener();
+
+AMQ_addStyle(`
+    .hiddenFriend {
+        background-color: lightblue;
+    }
+`);
+
+AMQ_addScriptData({
+    name: "Hidden Friends",
+    author: "Mxyuki",
+    description: `
+        <p>This script let you know who in your friends are hiding from you.</p>
+        <p>Opening the friend list will display your friends that are hidden like this :</p>
+        <img src="https://i.imgur.com/cLnMK0H.png">
+        <p>Doing ${command} in chat will display all your hidden friends in chat like this :</p>
+        <img src="https://i.imgur.com/H6HCnIL.png">
+        <p>And if noone is hiding and you do ${command} it will tell you that no one is hiding.</p>
+    `
+});
