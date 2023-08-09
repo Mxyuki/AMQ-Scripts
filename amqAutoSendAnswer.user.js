3fd91740d56e042cf3c689f105c9e2ae9791f9c4
Micookie <86327219+Mxyuki@users.noreply.github.com>
2023-08-09 21:34:07 +0200

    0.5

--- amqAutoSendAnswer.user.js
+++ amqAutoSendAnswer.user.js
@@ -6,7 +6,7 @@
 // @author       Mxyuki
 // @match        https://animemusicquiz.com/
 // ==/UserScript==
-if (document.getElementById("loginPage")) return
+if (document.getElementById("#loginPage")) return
 
 let isAutoSend = false;
 
