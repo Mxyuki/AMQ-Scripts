fbff55e05322275ffab2e9d6fe654bff5866482a
Micookie <86327219+Mxyuki@users.noreply.github.com>
2024-09-29 22:25:20 +0200

    1.0

--- amqCheckScriptVersionTest.js
+++ amqCheckScriptVersionTest.js
@@ -4,18 +4,13 @@ function checkScriptVersion(scriptName, scriptVersion) {
     if (!(scriptName in scriptVersionCheck)) {
         scriptVersionCheck[scriptName] = scriptVersion;
         localStorage.setItem('scriptVersionCheck', JSON.stringify(scriptVersionCheck));
-        console.log(`Added ${scriptName} with version ${scriptVersion} to local storage.`);
     } else {
         const storedVersion = scriptVersionCheck[scriptName];
         if (storedVersion !== scriptVersion) {
-            popoutMessages.displayStandardMessage(scriptName, `${scriptName} has been updated from version ${storedVersion} to ${scriptVersion}`);
-            console.log(`${scriptName} has been updated from version ${storedVersion} to ${scriptVersion}.`);
+            popoutMessages.displayStandardMessage(scriptName, `Updated from version ${storedVersion} → ${scriptVersion}`);
             scriptVersionCheck[scriptName] = scriptVersion;
             localStorage.setItem('scriptVersionCheck', JSON.stringify(scriptVersionCheck));
-        } else {
-            console.log(`${scriptName} is up to date (version ${scriptVersion}).`);
         }
     }
 }
-
 window.checkScriptVersion = checkScriptVersion;
