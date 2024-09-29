dca40d9ad86c77101ec3215be4499bcab87219bb
Micookie <86327219+Mxyuki@users.noreply.github.com>
2024-09-29 22:13:20 +0200

    Update amqCheckScriptVersion.js

--- amqCheckScriptVersion.js
+++ amqCheckScriptVersion.js
@@ -8,13 +8,15 @@ function checkScriptVersion(scriptName, scriptVersion) {
     } else {
         const storedVersion = scriptVersionCheck[scriptName];
         if (storedVersion !== scriptVersion) {
-            scriptVersionCheck[scriptName] = scriptVersion;
-            localStorage.setItem('scriptVersionCheck', JSON.stringify(scriptVersionCheck));
-
+            // Display the message before updating the version in local storage
             if (typeof popoutMessages !== 'undefined' && typeof popoutMessages.displayStandardMessage === 'function') {
                 popoutMessages.displayStandardMessage(scriptName, `${scriptName} has been updated from version ${storedVersion} to ${scriptVersion}`);
             }
             console.log(`${scriptName} has been updated from version ${storedVersion} to ${scriptVersion}.`);
+
+            // Now update the version in local storage
+            scriptVersionCheck[scriptName] = scriptVersion;
+            localStorage.setItem('scriptVersionCheck', JSON.stringify(scriptVersionCheck));
         } else {
             console.log(`${scriptName} is up to date (version ${scriptVersion}).`);
         }
