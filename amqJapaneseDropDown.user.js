6aad0d48ebc8b1603fa1d5f18416a5ee364f74e4
Micookie <86327219+Mxyuki@users.noreply.github.com>
2024-11-02 18:51:57 +0100

    1.4.0

--- amqJapaneseDropDown.user.js
+++ amqJapaneseDropDown.user.js
@@ -1,14 +1,15 @@
 // ==UserScript==
 // @name         AMQ Japanese DropDown
 // @namespace    https://github.com/Mxyuki/AMQ-Scripts
-// @version      1.3
-// @description  Make AMQ playable using Japanese characters
-// @description  I scraped ANN for the titles, and there might be Missing anime because the names didn't matched title in amq, or some didn't got scrapped for some reason.
-// @description  Most scrapped info from ANN are accessible here https://github.com/Mxyuki/amqJP
-// @description  If you want to upgrade it feel free to use anything i put here
+// @version      1.4.0
+// @description  Make AMQ playable using Japanese characters.
+// @description  Most scrapped info from ANN are accessible here https://github.com/Mxyuki/amqJP <- I need to edit this to match the new way.
+// @description  If you want to upgrade it feel free to use anything I put there.
+// @description  Remade the way I get the titles usingthe new Expand Library, still need some tweakingto be perfect but should be working  better now.
 // @author       Myuki
 // @match        https://animemusicquiz.com/*
 // @icon         https://i.imgur.com/syptORo.png
+// @require      https://github.com/Mxyuki/AMQ-Scripts/raw/refs/heads/main/amqCheckScriptVersion.js
 // @downloadURL  https://raw.githubusercontent.com/Mxyuki/AMQ-Scripts/main/amqJapaneseDropDown.user.js
 // @updateURL    https://raw.githubusercontent.com/Mxyuki/AMQ-Scripts/main/amqJapaneseDropDown.user.js
 // ==/UserScript==
@@ -16,14 +17,17 @@
 (function() {
     'use strict';
 
-    const githubJsonUrl = 'https://raw.githubusercontent.com/Mxyuki/amqJP/main/dropDown.json';
+    let version = "1.4.0";
+    checkScriptVersion("AMQ Japanese DropDown", version);
+
+    const githubJsonUrl = 'https://raw.githubusercontent.com/Mxyuki/amqJP/main/newDropDown.json';
     let jsonData = null;
 
     async function fetchJsonData() {
         try {
             let response = await fetch(githubJsonUrl);
             if (!response.ok) {
-                throw new Error('Network response was not ok ' + response.statusText);
+                throw new Error('Network response was not ok: ' + response.statusText);
             }
             jsonData = await response.json();
         } catch (error) {
@@ -31,13 +35,13 @@
         }
     }
 
-    async function convertJapaneseToMain(japaneseTitle) {
+    async function convertJapaneseToRO(japaneseTitle) {
         if (!jsonData) {
             await fetchJsonData();
         }
         for (let item of jsonData) {
-            if (item.japanese_title === japaneseTitle) {
-                return item.main_title;
+            if (item.JA === japaneseTitle) {
+                return item.RO;
             }
         }
         return japaneseTitle;
@@ -46,8 +50,7 @@
     let originalSubmitAnswer = QuizTypeAnswerInputController.prototype.submitAnswer;
 
     QuizTypeAnswerInputController.prototype.submitAnswer = async function(answer) {
-        let convertedAnswer = await convertJapaneseToMain(answer);
-        console.log(`Converted answer: ${convertedAnswer}`);
+        let convertedAnswer = await convertJapaneseToRO(answer);
         originalSubmitAnswer.call(this, convertedAnswer);
     };
 
@@ -59,7 +62,7 @@
                     await fetchJsonData();
                 }
                 if (Array.isArray(jsonData)) {
-                    this.list = jsonData.map(item => item.japanese_title);
+                    this.list = jsonData.map(item => item.JA);
                 } else {
                     console.error('Invalid data format:', jsonData);
                     return;
@@ -94,24 +97,36 @@
     };
 
     new Listener("answer results", async (payload) => {
-        let name = payload.songInfo.altAnimeNames;
-        if (Array.isArray(name)) {
+        if (payload.songInfo && payload.songInfo.animeNames) {
+            const { romaji, english } = payload.songInfo.animeNames;
+
             if (!jsonData) {
                 await fetchJsonData();
             }
-            for (let animeName of name) {
-                let found = false;
+
+            let foundJA = null;
+
+            if (romaji) {
                 for (let item of jsonData) {
-                    if (item.main_title === animeName) {
-                         $('#qpAnimeName').text(item.japanese_title);
-                        fitTextToContainer($("#qpAnimeName"), $("#qpAnimeNameContainer"), 25, 11);
-                        found = true;
+                    if (item.RO === romaji) {
+                        foundJA = item.JA;
                         break;
                     }
                 }
-                if (found) {
-                    break;
+            }
+            if (!foundJA && english) {
+                for (let item of jsonData) {
+                    if (item.EN === english) {
+                        foundJA = item.JA;
+                        break;
+                    }
                 }
+            }
+            if (foundJA) {
+                setTimeout(() => {
+                    $('#qpAnimeName').text(foundJA);
+                    fitTextToContainer($("#qpAnimeName"), $("#qpAnimeNameContainer"), 25, 11);
+                }, 100);
             }
         }
     }).bindListener();
